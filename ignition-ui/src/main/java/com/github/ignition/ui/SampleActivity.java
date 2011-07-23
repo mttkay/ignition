effbf1023d61244ffcbc68a60545a2b313e20504
Matthias Kaeppler <m.kaeppler@gmail.com>
2011-07-23 13:17:01 +0200

    add sample UI module for testing

---
+++ ignition-ui/src/main/java/com/github/ignition/ui/SampleActivity.java
@@ -0,0 +1,14 @@
+package com.github.ignition.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SampleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ig_main);
+    }
+
+}
