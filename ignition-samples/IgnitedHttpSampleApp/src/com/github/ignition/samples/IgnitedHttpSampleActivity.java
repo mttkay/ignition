a4c226f28dc9492910c700148e5400fa769a8edf
Matthias Käppler <m.kaeppler@gmail.com>
2012-01-13 15:16:31 +0100

    More work on the sample app for HTTP and response caching

--- ignition-samples/IgnitedHttpSampleApp/src/com/github/ignition/samples/IgnitedHttpSampleActivity.java
+++ ignition-samples/IgnitedHttpSampleApp/src/com/github/ignition/samples/IgnitedHttpSampleActivity.java
@@ -1,11 +1,15 @@
 package com.github.ignition.samples;
 
+import java.io.File;
+import java.util.List;
+
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,19 +21,36 @@ import com.github.ignition.support.http.cache.HttpResponseCache;
 
 public class IgnitedHttpSampleActivity extends Activity {
 
-    private ImageView imageView;
     private TextView statusText;
-    
-    private IgnitedHttp http;    
-    
+    private CheckBox useCache;
+
+    private IgnitedHttp http;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        imageView = (ImageView) findViewById(R.id.image);
         statusText = (TextView) findViewById(R.id.status);
+        useCache = (CheckBox) findViewById(R.id.chk_use_cache);
+        useCache.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                enableCache(isChecked);
+                updateCacheStatus();
+            }
+        });
+
         http = new IgnitedHttp(this);
-        http.enableResponseCache(this, 5, 30, 1, HttpResponseCache.DISK_CACHE_INTERNAL);        
+        enableCache(useCache.isChecked());
+    }
+
+    private void enableCache(boolean isChecked) {
+        if (isChecked) {
+            http.enableResponseCache(IgnitedHttpSampleActivity.this, 3, 30, 1,
+                    HttpResponseCache.DISK_CACHE_INTERNAL);
+        } else {
+            http.disableResponseCache(true);
+        }
     }
 
     public void onDownloadButtonClicked(View button) {
@@ -37,81 +58,85 @@ public class IgnitedHttpSampleActivity extends Activity {
         task.connect(this);
         task.execute();
     }
-    
+
+    public void onClearAll(View button) {
+        http.getResponseCache().clear();
+        updateCacheStatus();
+    }
+
+    public void onClearMemory(View button) {
+        http.getResponseCache().clear(false);
+        updateCacheStatus();
+    }
+
     public void updateStatusText(String message) {
         statusText.setText(message);
     }
 
-    public void updateCacheStatus(String url, boolean cachedResponse) {
+    public void updateStatusText(String url, boolean cachedResponse) {
+        if (cachedResponse) {
+            statusText.setText("File served from cache");
+        } else {
+            statusText.setText("File downloaded from Web");
+        }
+    }
+
+    public void updateCacheStatus() {
         EditText memoryCacheText = (EditText) findViewById(R.id.text_cache_memory);
         memoryCacheText.setText("");
         EditText diskCacheText = (EditText) findViewById(R.id.text_cache_disk);
         diskCacheText.setText("");
-        
+
         HttpResponseCache cache = http.getResponseCache();
-        
-        for (String key : cache.keySet()) {
-            if (cache.containsKeyInMemory(key)) {
-                memoryCacheText.append(key + "\n");
-            } else if (cache.containsKey(key)) {
-                diskCacheText.append(key + "\n");
+        if (cache != null) {
+            for (String key : cache.keySet()) {
+                if (cache.containsKeyInMemory(key)) {
+                    memoryCacheText.append(cache.getFileNameForKey(key) + "\n");
+                }
+            }
+            List<File> cachedFiles = cache.getCachedFiles();
+            for (File file : cachedFiles) {
+                diskCacheText.append(file.getName() + "\n");
             }
         }
-        
-        if (cachedResponse) {
-            if (cache.containsKeyInMemory(url)) {
-                statusText.setText("Image served from in-memory cache");
-            } else if (cache.containsKey(url)) {
-                statusText.setText("Image served from disk cache");
-            }
-        } else {
-            statusText.setText("Image downloaded from Web");
-        }
-        
     }
-    
-    public void updateImageView(Bitmap bitmap) {
-        imageView.setImageBitmap(bitmap);
-    }
-    
+
     public static final class HttpTask extends
             IgnitedAsyncTask<IgnitedHttpSampleActivity, Void, Void, IgnitedHttpResponse> {
 
         private IgnitedHttp http;
-        
+
         private String url = "http://developer.android.com/images/home/android-design.png";
 
         public HttpTask(IgnitedHttp http) {
             this.http = http;
         }
-        
+
         @Override
         protected void onStart(IgnitedHttpSampleActivity context) {
-            context.updateStatusText("Downloading image...");
+            context.updateStatusText("Downloading file...");
         }
 
         @Override
         protected IgnitedHttpResponse run(Void... params) throws Exception {
-            IgnitedHttpResponse response = http.get(url, true).retries(3).send();
-            return response;
+            return http.get(url, true).retries(3).expecting(200).send();
         }
 
         @Override
         protected void onError(IgnitedHttpSampleActivity context, Exception error) {
             super.onError(context, error); // prints a stack trace
 
-            context.updateStatusText("Download failed!");
-            
+            context.updateStatusText("Download failed! " + error.getCause().getMessage());
+
             Toast.makeText(context, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
         }
 
         @Override
         protected void onSuccess(IgnitedHttpSampleActivity context, IgnitedHttpResponse response) {
-            //context.updateImageView(BitmapFactory.decodeStream(response.getResponseBody()));
-            
             boolean cachedResponse = response instanceof CachedHttpResponse;
-            
-            context.updateCacheStatus(url, cachedResponse);
+
+            context.updateStatusText(url, cachedResponse);
+            context.updateCacheStatus();
         }
     }
 }
