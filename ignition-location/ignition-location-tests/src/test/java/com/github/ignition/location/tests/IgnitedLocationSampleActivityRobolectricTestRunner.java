e481ac8a494517fd0e24b53ddd97da864ebd04ba
Stefano Dacchille <stefano@qype.com>
2012-01-27 17:05:35 +0100

    Renamed ignition-location robolectric test runner and updated path to AndroidManifest file

---
+++ ignition-location/ignition-location-tests/src/test/java/com/github/ignition/location/tests/IgnitedLocationSampleActivityRobolectricTestRunner.java
@@ -0,0 +1,14 @@
+package com.github.ignition.location.tests;
+
+import java.io.File;
+
+import org.junit.runners.model.InitializationError;
+
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+
+public class IgnitedLocationSampleActivityRobolectricTestRunner extends RobolectricTestRunner {
+
+    public IgnitedLocationSampleActivityRobolectricTestRunner(Class<?> testClass) throws InitializationError {
+        super(testClass, new File("../ignition-location-samples/IgnitedLocationSampleActivity/"));
+    }
+}
