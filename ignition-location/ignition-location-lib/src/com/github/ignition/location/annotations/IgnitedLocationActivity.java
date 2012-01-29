dd3a20470b8ebf2d071e2eaa57da51a9751e73d9
Stefano Dacchille <stefano.dacchille@gmail.com>
2012-01-29 18:54:33 +0100

    Added documentation for IgnitedLocationActivity

--- ignition-location/ignition-location-lib/src/com/github/ignition/location/annotations/IgnitedLocationActivity.java
+++ ignition-location/ignition-location-lib/src/com/github/ignition/location/annotations/IgnitedLocationActivity.java
@@ -26,23 +26,90 @@ import com.github.ignition.location.IgnitedLocationConstants;
 @Target({ ElementType.TYPE })
 public @interface IgnitedLocationActivity {
 
+    /**
+     * Determines whether to use GPS when requesting location updates or not. Default value is
+     * {@link IgnitedLocationConstants.USE_GPS_DEFAULT}).
+     *
+     * @return true if GPS is used when requesting location updates, false otherwise.
+     */
     boolean useGps() default IgnitedLocationConstants.USE_GPS_DEFAULT;
 
+    /**
+     * Determines whether location updates should be requested or not. Default value is
+     * {@link IgnitedLocationConstants.REQUEST_LOCATION_UPDATES_DEFAULT}).
+     *
+     * @return true if location updates should be requested, false otherwise.
+     */
     boolean requestLocationUpdates() default IgnitedLocationConstants.REQUEST_LOCATION_UPDATES_DEFAULT;
 
+    /**
+     * Determines the minimum difference (in meters) between two location updates. Default value is
+     * {@link IgnitedLocationConstants.LOCATION_UPDATES_DISTANCE_DIFF_DEFAULT}).
+     *
+     * @return the minimum difference (in meters) between two location updates.
+     */
     int locationUpdatesDistanceDiff() default IgnitedLocationConstants.LOCATION_UPDATES_DISTANCE_DIFF_DEFAULT;
 
+    /**
+     * Determines the minimum interval (in milliseconds) between two location updates. Default value is
+     * {@link IgnitedLocationConstants.LOCATION_UPDATES_INTERVAL_DEFAULT}).
+     *
+     * @return the minimum interval (in milliseconds) between two location updates.
+     */
     long locationUpdatesInterval() default IgnitedLocationConstants.LOCATION_UPDATES_INTERVAL_DEFAULT;
 
+    /**
+     * Determines the minimum difference (in meters) between two location updates. Default value is
+     * {@link IgnitedLocationConstants.PASSIVE_LOCATION_UPDATES_DISTANCE_DIFF_DEFAULT}).
+     *
+     * @return the minimum difference (in meters) between two location updates
+     */
     int passiveLocationUpdatesDistanceDiff() default IgnitedLocationConstants.PASSIVE_LOCATION_UPDATES_DISTANCE_DIFF_DEFAULT;
 
+    /**
+     * Determines the minimum interval (in milliseconds) between two location updates. Default value is
+     * {@link IgnitedLocationConstants.PASSIVE_LOCATION_UPDATES_INTERVAL_DEFAULT}).
+     *
+     * @return the minimum interval (in milliseconds) between two location updates.
+     */
     long passiveLocationUpdatesInterval() default IgnitedLocationConstants.PASSIVE_LOCATION_UPDATES_INTERVAL_DEFAULT;
 
+    /**
+     * Determines whether passive location updates should be enabled or not. Default value is
+     * {@link IgnitedLocationConstants.ENABLE_PASSIVE_LOCATION_UPDATES_DEFAULT}).
+     *
+     * @return true if passive location updates should be enabled, false otherwise.
+     */
     boolean enablePassiveUpdates() default IgnitedLocationConstants.ENABLE_PASSIVE_LOCATION_UPDATES_DEFAULT;
 
+    /**
+     * Determines the interval (in milliseconds) to wait for a GPS fix. If a fix is not returned
+     * within this interval GPS updates will be disabled and updates from the network will be
+     * requested instead. Default value is
+     * {@link IgnitedLocationConstants.WAIT_FOR_GPS_FIX_INTERVAL_DEFAULT}).
+     *
+     * @return the interval (in milliseconds) to wait for a GPS fix.
+     */
     long waitForGpsFix() default IgnitedLocationConstants.WAIT_FOR_GPS_FIX_INTERVAL_DEFAULT;
 
+    /**
+     * Determines the minimum battery level to enable location updates using GPS. Default value is
+     * {@link IgnitedLocationConstants.MIN_BATTERY_LEVEL_DEFAULT}).
+     *
+     * @return the minimum battery level to enable location updates using GPS.
+     */
     int minBatteryLevel() default IgnitedLocationConstants.MIN_BATTERY_LEVEL_DEFAULT;
 
+    /**
+     * Determines whether the activity should display a "wait for location" dialog while the
+     * location manager is trying to get a location. Default default value is
+     * {@link IgnitedLocationConstants.SHOW_WAIT_FOR_LOCATION_DIALOG_DEFAULT}).
+     *
+     * NB: if this is true, the Activity MUST create a dialog in
+     * {@link android.app.Activity#onCreateDialog(int, android.os.Bundle)}
+     *
+     * @return true if the activity should display a "wait for location" dialog while the location
+     *         manager is trying to get a location, false otherwise.
+     */
     boolean showWaitForLocationDialog() default IgnitedLocationConstants.SHOW_WAIT_FOR_LOCATION_DIALOG_DEFAULT;
 }
