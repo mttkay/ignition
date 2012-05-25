35fa6d0192463d1efa73213ecb54e71a940970dd
Stefano Dacchille <stefano@qype.com>
2012-05-25 18:20:16 +0200

    Compilation error fix in location sample activity

--- ignition-location/ignition-location-samples/src/com/github/ignition/samples/location/ui/IgnitedLocationSampleActivity.java
+++ ignition-location/ignition-location-samples/src/com/github/ignition/samples/location/ui/IgnitedLocationSampleActivity.java
@@ -236,7 +236,7 @@ public class IgnitedLocationSampleActivity extends MapActivity {
     @Override
     protected Dialog onCreateDialog(int id) {
         if (id == R.id.ign_loc_dialog_wait_for_fix) {
-            return IgnitedLocationSupport.createWaitForLocationDialog(this).create();
+            return IgnitedLocationSupport.createWaitForLocationDialog(this);
         } else if (id == R.id.ign_loc_dialog_no_providers_enabled) {
             return IgnitedLocationSupport.createNoProvidersEnabledDialog(this, true).create();
         }
